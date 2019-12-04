442ddf59429332265a6bcd84427785d85c3f9da5
Tommy Berman <tomy.berman1@gmail.com>
2019-12-04 23:15:03 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -27,7 +27,7 @@ end
 
 def list_of_directors(source)
   # Write this implementation
-  pp source
+  
 end
 
 def total_gross(source)
@@ -40,6 +40,7 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+  pp source
 end
 
 
